c2d867389bf096130feeeaebf7c57bbfdde03d9c
kinhvin <kevintruong.code@gmail.com>
2024-10-19 13:08:09 -0400

    3. Update Central Layout (Points: 7)

--- client/src/layouts/Central/Central.tsx
+++ client/src/layouts/Central/Central.tsx
@@ -11,6 +11,7 @@ interface Props {
 }
 
 function Central({ title, children }: Props) {
+
   return (
     <Base>
       <Banner />
@@ -22,10 +23,21 @@ function Central({ title, children }: Props) {
         <div className="Central__page__content">{children}</div>
         <Divider />
         <div className="Central__page__footer">
+
+          <div className="Central__page__footer__contact">
+            <a href="https://carleton.ca/about/contact/" target="_blank">Contact Information</a>
+          </div>
+
+          <div className="Central__page__footer__address">
+            <p>1125 Colonel By Dr, Ottawa, ON K1S 5B6</p>
+          </div>
+
           <div className="Central__page__footer__release">Release: 8.10.1</div>
+
           <div className="Central__page__footer__copyright">
             © 2024 Ellucian Company L.P. and its affiliates.
           </div>
+
         </div>
       </div>
     </Base>
